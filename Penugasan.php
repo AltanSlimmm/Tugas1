e04f1683046bffd2256ddee095b6c43cc4052a5d
AltanSlimmm <126134793+AltanSlimmm@users.noreply.github.com>
2023-04-04 18:57:31 +0700

    Create Penugasan.php

---
+++ Penugasan.php
@@ -0,0 +1,17 @@
+<?php
+
+$TotalHarga = 0;
+
+$Gday = 4000;
+$Signature = 2000;
+$Magnum = 2500;
+$Garpit = 2500;
+
+$TotalHarga += $Gday;
+$TotalHarga += $Signature;
+$TotalHarga += $Magnum;
+$TotalHarga += $Garpit;
+
+var_dump($TotalHarga);
+
+?>
