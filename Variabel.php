28ca475912893b3a9973fc11a1aef3e47fb31927
AltanSlimmm <126134793+AltanSlimmm@users.noreply.github.com>
2023-04-04 19:00:17 +0700

    Create Variabel.php

---
+++ Variabel.php
@@ -0,0 +1,20 @@
+<?php
+
+$name = "Sulthan SRA";
+$usia = 20;
+$alamat = "Kp. Laksanamekar";
+
+
+echo "Nama : ";
+echo $name;
+echo "\n";
+
+echo "Usia :";
+echo $usia;
+echo "\n";
+
+echo "alamat :";
+echo $alamat;
+
+
+?>
