c907b07663845466eb6d11458c6c03279930512a
AltanSlimmm <126134793+AltanSlimmm@users.noreply.github.com>
2023-04-04 18:49:48 +0700

    Create Constant.php

---
+++ Constant.php
@@ -0,0 +1,17 @@
+<?php
+
+define ("PENYANYI", "Babang Ical");
+define ("JUDUL", "Senja Senja Bisa");
+
+echo "Menampilkan Musik Yang di Putar ";
+echo "\n";
+
+echo "Penyanyi :";
+echo PENYANYI;
+echo "\n";
+
+echo "Judul :";
+echo JUDUL;
+echo "\n";
+
+?>
